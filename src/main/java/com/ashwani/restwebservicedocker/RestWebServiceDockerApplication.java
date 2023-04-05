e8d371377db5af396db558584952254d2b0ff9a0
AshwaniKRawat <rawat.ashwani@gmail.com>
2023-04-05 09:06:13 +0530

    initial

---
+++ src/main/java/com/ashwani/restwebservicedocker/RestWebServiceDockerApplication.java
@@ -0,0 +1,13 @@
+package com.ashwani.restwebservicedocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebServiceDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWebServiceDockerApplication.class, args);
+	}
+
+}
