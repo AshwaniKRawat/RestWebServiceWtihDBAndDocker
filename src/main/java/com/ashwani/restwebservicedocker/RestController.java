c76a32c207ad354df18ec95a1227b929b531cae9
AshwaniKRawat <rawat.ashwani@gmail.com>
2023-04-05 14:51:18 +0530

    added html code into RestController

--- src/main/java/com/ashwani/restwebservicedocker/RestController.java
+++ src/main/java/com/ashwani/restwebservicedocker/RestController.java
@@ -8,6 +8,38 @@ public class RestController {
  @RequestMapping(method = RequestMethod.GET, path="/welcome")
   String helloWorld()
   {
-	 return "<h1><center>Hello World from docker</center</h1><br>This page is coming from docker<br>Created by Ashwani";
+	 //return "<h1><center>Hello World from docker</center</h1><br>This page is coming from docker<br>Created by Ashwani";
+	 return "<html>\r\n"
+	 		+ "<head>\r\n"
+	 		+ "<meta charset=\"ISO-8859-1\">\r\n"
+	 		+ "<title>Spring web application</title>\r\n"
+	 		+ "</head>\r\n"
+	 		+ "<body>\r\n"
+	 		+ "	<table border=1 style=\"height: 100%; width: 100%; background-color: black; color: white;\">\r\n"
+	 		+ "		<tr style=\"height: 10%\">\r\n"
+	 		+ "			<td></td>\r\n"
+	 		+ "			<td><center>\r\n"
+	 		+ "					<h1>Hello Docker</h1>\r\n"
+	 		+ "					<br><p><h3>Hello Maven + Spring MVC + Jenkins + Docker Example</h3></p><br>\r\n"
+	 		+ "				</center></td>\r\n"
+	 		+ "			<td></td>\r\n"
+	 		+ "		</tr>\r\n"
+	 		+ "		<tr  style=\"height: 80%\">\r\n"
+	 		+ "			<td></td>\r\n"
+	 		+ "			<td><center>\r\n"
+	 		+ "					<h2>Hello this page is coming from spring web application developed by  Ashwani Rawat.</h2> \r\n"
+	 		+ "				</center></td>\r\n"
+	 		+ "			<td></td>\r\n"
+	 		+ "		</tr>\r\n"
+	 		+ "		<tr  style=\"height: 10%\">\r\n"
+	 		+ "			<td></td>\r\n"
+	 		+ "			<td><center>\r\n"
+	 		+ "					@Ashwani 4 April 2023\r\n"
+	 		+ "				</center></td>\r\n"
+	 		+ "			<td></td>\r\n"
+	 		+ "		</tr>\r\n"
+	 		+ "	</table>\r\n"
+	 		+ "</body>\r\n"
+	 		+ "</html>";
   }
 }
